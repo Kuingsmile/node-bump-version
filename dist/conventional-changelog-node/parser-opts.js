216717a271070f2ffe1e4bef18de7469709a8658
Kuingsmile <96409857+Kuingsmile@users.noreply.github.com>
2026-09-12 00:23:30 -0700

    :package: Chore: untrack dist dir

--- dist/conventional-changelog-node/parser-opts.js
+++
@@ -1,8 +0,0 @@
-const parserOpts = {
-    headerPattern: /^(:.*: \w*)(?:\((.*)\))?: (.*)$/,
-    headerCorrespondence: ['type', 'scope', 'subject'],
-    noteKeywords: ['BREAKING CHANGE']
-};
-
-export { parserOpts as default };
-//# sourceMappingURL=parser-opts.js.map
